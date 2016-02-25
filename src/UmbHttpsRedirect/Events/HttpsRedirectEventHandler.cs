a6d23f6319eb2961d819fc183e9a10eba2497948
bitmapped <bpowell@bitmapped.net>
2016-02-25 11:34:57 -0500

    Initial implementation

--- src/UmbHttpsRedirect/Events/HttpsRedirectEventHandler.cs
+++ src/UmbHttpsRedirect/Events/HttpsRedirectEventHandler.cs
@@ -3,11 +3,120 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using UmbHttpsRedirect.Support;
 using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Web;
+using Umbraco.Web.Routing;
 
 namespace UmbHttpsRedirect.Events
 {
     public class HttpsRedirectEventHandler : ApplicationEventHandler
     {
+        /// <summary>
+        /// Configuration settings.
+        /// </summary>
+        private readonly Settings settings;
+
+        /// <summary>
+        /// Constructor to load settings.
+        /// </summary>
+        public HttpsRedirectEventHandler()
+        {
+            // Load settings from config file.
+            this.settings = new Settings();
+        }
+
+        /// <summary>
+        /// Register event handler on start.
+        /// </summary>
+        /// <param name="httpApplicationBase">Umbraco application.</param>
+        /// <param name="applicationContext">Application context.</param>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            PublishedContentRequest.Prepared += PublishedContentRequest_Prepared;
+        }
+
+        /// <summary>
+        /// Event handler to redirect traffic to HTTPS.
+        /// </summary>
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Event args</param>
+        private void PublishedContentRequest_Prepared(object sender, EventArgs e)
+        {
+            // Get request.
+            var request = sender as PublishedContentRequest;
+            var context = HttpContext.Current;
+
+            // If the response is invalid, the page doesn't exist, or will be changed already, don't do anything more.
+            if ((request == null) || (!request.HasPublishedContent) || (request.Is404) || (request.IsRedirect) || (request.ResponseStatusCode > 0))
+            {
+                // Log for debugging.
+                LogHelper.Debug<HttpsRedirectEventHandler>("Stopping HttpsRedirect for requested URL {0} because request was null ({1}), there was no published content ({2}), was 404 ({3}), was a redirect ({4}), or status code ({5}) was already set.",
+                    () => context.Request.Url.AbsolutePath,
+                    () => (request == null),
+                    () => (!request.HasPublishedContent),
+                    () => (request.Is404),
+                    () => (request.IsRedirect),
+                    () => request.ResponseStatusCode);
+
+                return;
+            }
+
+            // Determine if page should be redirected. Check page ID, property, and document type.
+            if (request.PublishedContent.GetPropertyValue<bool>("umbHttpsRedirect", false) || settings.PageIds.Contains(request.PublishedContent.Id) || settings.DocTypes.Contains(request.PublishedContent.DocumentTypeAlias))
+            {
+                // See if we're not secure already.
+                if (!context.Request.IsSecureConnection)
+                {
+                    // We need to redirect.
+
+                }
+            }
+        }
+
+        private void Redirect(HttpContext context, bool redirectToHttps)
+        {
+            // Get the existing Url.
+            var existingUrl = context.Request.Url;
+
+            // Construct new url.
+            var uriBuilder = new UriBuilder()
+            {
+                Host = existingUrl.Host,
+                Path = existingUrl.AbsolutePath,
+                Query = existingUrl.Query,
+                Fragment = existingUrl.Fragment
+            };
+
+            // Determine if we are redirecting to HTTPs.
+            if (redirectToHttps)
+            {
+                uriBuilder.Scheme = "https";
+                uriBuilder.Port = settings.HttpsPort ?? -1; // Use default port (-1) if port was not specified.
+            }
+            else
+            {
+                uriBuilder.Scheme = "http";
+                uriBuilder.Port = settings.HttpPort ?? -1; // Use default port (-1) if port was not specified.
+            }
+
+            // Log that we are redirecting.
+            LogHelper.Debug<HttpsRedirectEventHandler>("Redirecting from {0} to {1} using {2} redirect.",
+                            () => existingUrl.AbsoluteUri,
+                            () => uriBuilder.Uri.AbsoluteUri,
+                            () => (settings.UseTemporaryRedirects ? "temporary" : "permanent"));
+
+            // Perform redirect.
+            if (settings.UseTemporaryRedirects)
+            {
+                context.Response.Redirect(uriBuilder.Uri.AbsoluteUri);
+            }
+            else
+            {
+                context.Response.RedirectPermanent(uriBuilder.Uri.AbsoluteUri);
+            }
+        }
     }
 }
--- src/UmbHttpsRedirect/Support/Settings.cs
+++ src/UmbHttpsRedirect/Support/Settings.cs
@@ -13,6 +13,11 @@ namespace UmbHttpsRedirect.Support
         // Define constants.
         private const string AppKey_DocTypes = "HttpsRedirect:DocTypes";
         private const string AppKey_PageIds = "HttpsRedirect:PageIds";
+        private const string AppKey_Templates = "HttpsRedirect:Templates";
+        private const string AppKey_ForceHttp = "HttpsRedirect:ForceHttp";
+        private const string AppKey_HttpPort = "HttpsRedirect:HttpPort";
+        private const string AppKey_HttpsPort = "HttpsRedirect:HttpsPort";
+        private const string AppKey_UseTemporaryRedirects = "HttpsRedirect:UseTemporaryRedirects";
 
         /// <summary>
         /// Load settings from configuration file.
@@ -20,8 +25,13 @@ namespace UmbHttpsRedirect.Support
         public Settings()
         {
             // Load values from config files.
-            this.DocTypes = this.ConfigLoadDocTypes();
-            this.PageIds = this.ConfigLoadPageIds();
+            this.DocTypes = this.ConfigLoadStringList(AppKey_DocTypes);
+            this.Templates = this.ConfigLoadStringList(AppKey_Templates);
+            this.PageIds = this.ConfigLoadIntList(AppKey_PageIds);
+            this.ForceHttp = this.ConfigLoadBool(AppKey_ForceHttp, false);
+            this.UseTemporaryRedirects = this.ConfigLoadBool(AppKey_UseTemporaryRedirects, false);
+            this.HttpPort = this.ConfigLoadNullableInt(AppKey_HttpPort);
+            this.HttpsPort = this.ConfigLoadNullableInt(AppKey_HttpsPort);
         }
 
         /// <summary>
@@ -35,13 +45,39 @@ namespace UmbHttpsRedirect.Support
         public IEnumerable<string> DocTypes { get; private set; }
 
         /// <summary>
-        /// Load PageIds from configuration file.
+        /// Templates to force to be HTTPS.
         /// </summary>
-        /// <returns>List of PageIds.</returns>
-        private List<int> ConfigLoadPageIds()
+        public IEnumerable<string> Templates { get; private set; }
+
+        /// <summary>
+        /// Force HTTP is page is not specifically set to be HTTPS. Default/false is not to force redirect to HTTP.
+        /// </summary>
+        public bool ForceHttp { get; private set; }
+
+        /// <summary>
+        /// Use temporary 302 redirects rather than permanent 301 redirects.
+        /// </summary>
+        public bool UseTemporaryRedirects { get; private set; }
+
+        /// <summary>
+        /// Optional port to use for redirecting to HTTP.
+        /// </summary>
+        public int? HttpPort { get; private set; }
+
+        /// <summary>
+        ///  Optional prot to use for redirecting to HTTPS.
+        /// </summary>
+        public int? HttpsPort { get; private set; }
+
+        /// <summary>
+        /// Load comma-separated list of ints from configuration file.
+        /// </summary>
+        /// /// <param name="key">Key to load.</param>
+        /// <returns>List of values.</returns>
+        private List<int> ConfigLoadIntList(string key)
         {
             // Return empty list if there are no values.
-            if (WebConfigurationManager.AppSettings[AppKey_PageIds] == null)
+            if (WebConfigurationManager.AppSettings[key] == null)
             {
                 return new List<int>();
             }
@@ -49,48 +85,98 @@ namespace UmbHttpsRedirect.Support
             try
             {
                 // Split Page IDs into parts.
-                var pageIds = WebConfigurationManager.AppSettings[AppKey_PageIds]
+                var pageIds = WebConfigurationManager.AppSettings[key]
                     .Split(',')
                     .Select(x => x.Trim())
-                    .Select(x => Convert.ToInt32(x))
+                    .Select(x => int.Parse(x))
                     .ToList();
 
                 return pageIds;
             }
             catch
             {
-                throw new ConfigurationErrorsException("Value for " + AppKey_PageIds + " not correctly specified.");
+                throw new ConfigurationErrorsException(String.Format("Value for {0} not correctly specified.", key));
             }
 
         }
 
         /// <summary>
-        /// Load DocTypes from configuration file.
+        /// Load comma-separated list of strings from configuration file.
         /// </summary>
-        /// <returns>List of DocTypes.</returns>
-        private List<string> ConfigLoadDocTypes()
+        /// <param name="key">Key to load.</param>
+        /// <returns>List of values.</returns>
+        private List<string> ConfigLoadStringList(string key)
         {
             // Return empty list if there are no values.
-            if (WebConfigurationManager.AppSettings[AppKey_DocTypes] == null)
+            if (WebConfigurationManager.AppSettings[key] == null)
             {
                 return new List<string>();
             }
 
             try
             {
-                // Split DocTypes into parts.
-                var docTypes = WebConfigurationManager.AppSettings[AppKey_DocTypes]
+                // Split list into parts.
+                var values = WebConfigurationManager.AppSettings[key]
                     .Split(',')
                     .Select(x => x.Trim())
                     .ToList();
 
-                return docTypes;
+                return values;
             }
             catch
             {
-                throw new ConfigurationErrorsException("Value for " + AppKey_DocTypes + " not correctly specified.");
+                throw new ConfigurationErrorsException(String.Format("Value for {0} not correctly specified.", key));
+            }
+        }
+
+        /// <summary>
+        /// Load configuration value for boolean.
+        /// </summary>
+        /// <param name="key">Key to load.</param>
+        /// <param name="defaultValue">Default value to return if key is not specified.</param>
+        /// <returns>True/false.</returns>
+        private bool ConfigLoadBool(string key, bool defaultValue)
+        {
+            // If key was not set, default to false.
+            if (WebConfigurationManager.AppSettings[key] == null)
+            {
+                return defaultValue;
             }
 
+            // Value was set. Test it.
+            try
+            {
+                return Boolean.Parse(WebConfigurationManager.AppSettings[key]);
+            }
+            catch
+            {
+                throw new ConfigurationErrorsException(String.Format("Value for {0} not correctly specified.", key));
+            }
+        }
+
+        /// <summary>
+        /// Load configuration value for nullable integer.
+        /// </summary>
+        /// <param name="key">Key to load.</param>
+        /// <returns>Integer or null.</returns>
+        private int? ConfigLoadNullableInt(string key)
+        {
+            // If key was not set, return null.
+            // If key was not set, default to false.
+            if (WebConfigurationManager.AppSettings[key] == null)
+            {
+                return null;
+            }
+
+            // Value was set. Test it.
+            try
+            {
+                return Int32.Parse(WebConfigurationManager.AppSettings[key]);
+            }
+            catch
+            {
+                throw new ConfigurationErrorsException(String.Format("Value for {0} not correctly specified.", key));
+            }
         }
     }
 }
